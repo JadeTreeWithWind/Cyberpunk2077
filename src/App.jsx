e39e44f38f5a33b0462fe85402535c7f6d9ab147
TJ <tj.wang1995@mosji.co>
2026-01-20 20:20:42 +0800

    feat: 建立 App 元件並更新 favicon 與頁面標題。

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <main>
-      <h1 className="text-4xl underline font-bold">hello world</h1>
+      <h1 className="text-7xl text-violet-300 underline font-bold">
+        hello world
+      </h1>
     </main>
   );
 }
